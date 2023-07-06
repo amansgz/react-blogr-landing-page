e8474bca39152e73391d08cd81180b5234080a03
amansgz <amansgz.ar@gmail.com>
2023-07-06 14:08:01 -0300

    Fix typos

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,12 +3,12 @@ import bgPatternIntroDesktop from "./bg-pattern-intro-desktop.svg";
 import bgPatternIntroMobile from "./bg-pattern-intro-mobile.svg";
 import iconArrowDark from "./icon-arrow-dark.svg";
 import iconArrowLight from "./icon-arrow-light.svg";
-import iconClose from "./icon-close.svg"
+import iconClose from "./icon-close.svg";
 import iconHamburger from "./icon-hamburger.svg";
 import illustrationEditorDesktop from "./illustration-editor-desktop.svg";
-import illustrationEditorMobile from "./illustration-editor-mobile.svg"
+import illustrationEditorMobile from "./illustration-editor-mobile.svg";
 import illustrationLaptopDesktop from "./illustration-laptop-desktop.svg";
-import illustrationLaptopMobile "./illustration-laptop-mobile.svg";
+import illustrationLaptopMobile from "./illustration-laptop-mobile.svg";
 import illustrationPhones from "./illustration-phones.svg";
 import logo from "./logo.svg";
 
@@ -25,5 +25,5 @@ export {
 	illustrationLaptopDesktop,
 	illustrationLaptopMobile,
 	illustrationPhones,
-	logo,
+	logo
 }
