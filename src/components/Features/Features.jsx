fe8058c31d22af8013435c1f7b1339fdd93e9f92
amansgz <amansgz.ar@gmail.com>
2023-07-24 11:11:05 -0300

    Update features section

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -2,18 +2,20 @@ import "./Features.css";
 import { illustrationLaptopMobile,illustrationLaptopDesktop } from "../../assets";
 
 const Features = () => (
-	<section>
-		<div className= "container">
-			<div className= "content-two-columns">
-				<picture className= "content__image">
-					<source srcSet= {illustrationLaptopDesktop}
-					media= "(min-width: 50em)"/>
-					<source srcSet= {illustrationLaptopMobile}
-					media= "(max-width: 50em)"/>
-					<img src= {illustrationLaptopMobile} alt= "Laptop"/>
-				</picture>
+	<section className= "section-features">
+		<div className= "container container-center">
+			<div className= "flex-sm-column-center flex-md-two-columns">
+				<div className= "item-image-laptop">
+					<picture>
+						<source srcSet= {illustrationLaptopDesktop}
+						media= "(min-width: 767px)"/>
+						<source srcSet= {illustrationLaptopMobile}
+						media= "(max-width: 767px)"/>
+						<img src= {illustrationLaptopMobile} alt= "Laptop"/>
+					</picture>
+				</div>
 
-				<div className= "content-info column-center column-md-left">
+				<div className= "item-content-features">
 					<h2 className= "title title-dark">
 						Free, open, simple
 					</h2>
@@ -22,7 +24,10 @@ const Features = () => (
 					features such as code syntax highlighting, RSS feeds, social media integration, third-party commenting tools, 
 					and works seamlessly with Google Analytics. The architecture is clean and is relatively easy to learn.
 					</p>
-					<h2 className= "title title-dark">
+
+					<div className= "spacer-large"></div>
+
+					<h2 className= "title title-dark md-heading">
 						Powerful tooling
 					</h2>
 					<p className= "text text-dark"> 	
