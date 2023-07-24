3895f3180acff56a6a4da4294d8723413e6d7405
amansgz <amansgz.ar@gmail.com>
2023-07-24 11:10:26 -0300

    Update design section

--- src/components/Design/Design.jsx
+++ src/components/Design/Design.jsx
@@ -2,21 +2,23 @@ import "./Design.css";
 import {illustrationEditorMobile, illustrationEditorDesktop} from "../../assets"
 
 const Design = () => (
-	<section className= "section-light">
+	<section className= "section-design">
 		<div className= "container">
 			<h2 className= "container-title title-dark">
 			Designed for the future
 			</h2>
-			<div className= "content-two-columns">
-				<picture className= "content__image">
-					<source srcSet= {illustrationEditorDesktop}
-					media= "(min-width: 50em)"/>
-					<source srcSet= {illustrationEditorMobile}
-					media= "(max-width: 50em)"/>
-					<img src= {illustrationEditorMobile} alt= "editor"/>
-				</picture>
+			<div className= "flex-sm-column-center flex-md-two-columns">
+				<div className= "item-image-editor">
+					<picture>
+						<source srcSet= {illustrationEditorDesktop}
+						media= "(min-width: 767px)"/>
+						<source srcSet= {illustrationEditorMobile}
+						media= "(max-width: 767px)"/>
+						<img src= {illustrationEditorMobile} alt= "editor"/>
+					</picture>
+				</div>	
 				
-				<div className= "content-info column-center column-md-left">
+				<div className= "item-content-design">
 					<h3 className= "title title-dark">
 						Introducing an extensible editor	
 					</h3>
@@ -24,6 +26,8 @@ const Design = () => (
 						Blogr features an exceedingly intuitive interface which lets you focus on one thing: creating content. The editor supports management of multiple blogs and allows easy manipulation of embeds such as images, videos, and Markdown. Extensibility with plugins and themes provide easy ways to add functionality or change the looks of a blog.
 					</p>	
 
+					<div className= "spacer-large"></div>
+					
 					<h3 className= "title title-dark">
 						Robust content management
 					</h3>
