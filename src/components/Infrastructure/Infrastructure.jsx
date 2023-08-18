b589ff4e84c0bfd6d2b034dcae86136f56003c36
amansgz <amansgz.ar@gmail.com>
2023-08-18 16:09:43 -0300

    Update styles

--- src/components/Infrastructure/Infrastructure.jsx
+++ src/components/Infrastructure/Infrastructure.jsx
@@ -4,14 +4,12 @@ import {illustrationPhones} from "../../assets";
 
 const Infrastructure = () => (
 	<section className= "section-infrastructure">
-	
 		<div className= "container-infrastructure flex-md-two-columns">
-			<div className= "images">
+		<div className= "images">
 				<picture>
 					<img src= {illustrationPhones} alt= "Phones"/>
 				</picture>
 			</div>	
-
 			<div className= "content">
 				<h2 className= "content__title">
 					State of the Art Infrastructure
@@ -21,6 +19,7 @@ const Infrastructure = () => (
 					This ensures your site will load instantly, no matter where your readers are, keeping your site competitive.
 				</p>
 			</div>
+		
 		</div>
 	
 	</section>
